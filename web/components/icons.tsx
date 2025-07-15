4d879110a31af58c874c0217010e4c54b037fe24
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2025-07-15 01:37:39 -0400

    Update icons.tsx

--- web/components/icons.tsx
+++ web/components/icons.tsx
@@ -26,26 +26,12 @@ export function CollectionIcon() {
   );
 }
 
-export function CloseIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="28"
-      height="28"
-      fill="#ffc6f5"
-      viewBox="2 2 12 12"
-    >
-      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-    </svg>
-  );
-}
-
 export function LockIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="22"
+      height="22"
       fill="currentColor"
       viewBox="0 0 16 16"
     >
